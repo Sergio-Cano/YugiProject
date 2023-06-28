50a48d82bd973e73ed73590a97a99e459b6c615a
Sergio-Cano <sergio95cano@hotmail.com>
2023-06-28 20:51:50 +0200

    feat: Back - Added index, DB config, docker yml, errors list and main folders

---
+++ back/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const errors = require("./misc/errors");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(routes(db));
+
+app.use((_, __, next) => {
+    next(errors[404])
+})
+
+app.use(({ statusCode, error }, _, res, __) => {
+    res.status(statusCode).json({
+        success: false,
+        message: error.message,
+    })
+})
+
+app.listen(process.env.PORT, () => console.log("> Listening at: ", process.env.PORT));
